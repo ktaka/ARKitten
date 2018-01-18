de98c589c7efb1ccf02f8b07754d9dfe687b6ced
Kenichi Takahashi <kenichi.takahashi@kabuku.co.jp>
2018-01-18 09:21:09 +0900

    Add a project for part 2.

---
+++ Assets/CatControl.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+public class CatControl : MonoBehaviour {
+    public Transform hitTransform;
+    private Quaternion rotateFrom;
+    private Quaternion rotateTo;
+    private float rotateDelta = 0.0f;
+    private const float rotateDuration = 3.0f;
+    private const float delayTime = 3.0f;
+
+    Vector3 GetLookVector () {
+        Vector3 lookVector = Camera.main.transform.position - hitTransform.transform.position;
+        lookVector.y = 0.0f; // Y         
+        lookVector.Normalize ();
+        return lookVector;
+    }
+
+    bool HitTestWithResultType (ARPoint point, ARHitTestResultType resultTypes)
+    {
+        List<ARHitTestResult> hitResults = UnityARSessionNativeInterface.GetARSessionNativeInterface ().HitTest (point, resultTypes);
+        if (hitResults.Count > 0) {
+            foreach (var hitResult in hitResults) {
+                Debug.Log ("Got hit!");
+                ResetRotateAnim ();
+                hitTransform.position = UnityARMatrixOps.GetPosition (hitResult.worldTransform);
+                hitTransform.rotation = Quaternion.LookRotation (GetLookVector());
+                return true;
+            }
+        }
+        return false;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0 && hitTransform != null) {
+            var touch = Input.GetTouch (0);
+            if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved) {
+                var screenPosition = Camera.main.ScreenToViewportPoint (touch.position);
+                ARPoint point = new ARPoint {
+                    x = screenPosition.x,
+                    y = screenPosition.y
+                };
+
+                // prioritize reults types
+                ARHitTestResultType[] resultTypes = {
+                    ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent, 
+                    // if you want to use infinite planes use this:
+                    //ARHitTestResultType.ARHitTestResultTypeExistingPlane,
+                    ARHitTestResultType.ARHitTestResultTypeHorizontalPlane, 
+                    ARHitTestResultType.ARHitTestResultTypeFeaturePoint
+                }; 
+
+                foreach (ARHitTestResultType resultType in resultTypes) {
+                    if (HitTestWithResultType (point, resultType)) {
+                        return;
+                    }
+                }
+            }
+        } else {
+            if (rotateDelta <= 0.0f) {
+                ResetRotateAnim ();
+                CheckObjDirection ();
+            } else {
+                RotateToCamera ();
+                rotateDelta -= Time.deltaTime;
+            }
+        }
+	}
+
+    void ResetRotateAnim () {
+        rotateDelta = 0.0f;
+    }
+
+    void CheckObjDirection () {
+        Vector3 objToCameraVec = GetLookVector ();
+        if (objToCameraVec == Vector3.zero) {
+            objToCameraVec = -Camera.main.transform.forward;
+        }
+        Vector3 objDirVector = hitTransform.forward;
+        float dot = Vector3.Dot (objToCameraVec, objDirVector);
+        if (dot <= 0.5f) {
+            rotateFrom = hitTransform.transform.rotation;
+            rotateTo = Quaternion.LookRotation (objToCameraVec, Vector3.up);
+            rotateDelta = rotateDuration + delayTime;
+        }
+    }
+
+    void RotateToCamera() {
+        if (rotateDelta <= rotateDuration) {
+            float t = 1.0f - (rotateDelta / rotateDuration); hitTransform.transform.rotation =
+                Quaternion.Slerp(rotateFrom, rotateTo, t);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ARKitten
 技術評論社の月刊誌Software Designの「ARKitとUnityで作るiPhone ARアプリ集中特講」で開発するサンプルのリポジトリです。
+
+[2018年2月号](http://gihyo.jp/magazine/SD/archive/2018/201802)の記事での完成状態がコミットされています。
+
+UnityのAsset Storeから[ARKitのプラグイン](http://u3d.as/RTd)と[Cute Kittenのアセット](http://u3d.as/cvs)をインポートしてからビルドしてください。
