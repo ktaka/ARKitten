3d4490ce3708858a40afad18d14417a5248c84f4
Kenichi Takahashi <kenichi.takahashi@kabuku.co.jp>
2018-02-07 22:07:03 +0900

    part 3-3:
    - add throwing behavior with flick operation.

--- Assets/BallOperation.cs
+++ Assets/BallOperation.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class BallOperation : MonoBehaviour {
+public class BallOperation : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
     public CatControl catControl;
     private int collisionCount = 0;
     private float yPosition;
+    private Rigidbody rb;
+    private Vector3 startPosition;
+    private Vector3 screenPos;
+    private float beginDragTime;
 
     void OnCollisionEnter(Collision co) {
         collisionCount++;
@@ -16,13 +21,47 @@ public class BallOperation : MonoBehaviour {
         yPosition = co.transform.position.y;
     }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody> ();
+        rb.useGravity = false;
+        yPosition = -Camera.main.farClipPlane;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if ((rb.position.y - yPosition) < 0) {
+            Destroy (gameObject, 1);
+        }
+    }
+
+    // オブジェクトをタップ
+    public void OnPointerDown (PointerEventData eventData) {
+        screenPos = Camera.main.WorldToScreenPoint (transform.position);
+        startPosition = transform.position;
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+        rb.AddForce (Vector3.zero);
+        collisionCount = 0;
+    }
+
+    // オブジェクトをドラッグ(フリック)開始
+    public void OnBeginDrag( PointerEventData data ) {
+        beginDragTime = Time.time;
+    }
+
+    // オブジェクトをドラッグしている間
+    public void OnDrag (PointerEventData eventData) {
+        Vector3 scrPos = eventData.position;
+        scrPos.z = screenPos.z;
+        Vector3 pos = Camera.main.ScreenToWorldPoint (scrPos);
+        transform.position = pos;
+    }
+    // オブジェクトのドラッグ終了(指を離した)
+    public void OnEndDrag (PointerEventData eventData) {
+        Vector3 moved = transform.position - startPosition;
+        float dragFactor = (Time.time - beginDragTime) * 10;
+        rb.AddForce (100.0f / dragFactor * moved);
+        rb.useGravity = true;
+    }
 }
