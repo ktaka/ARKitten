bc8034fa665ff8987d655d5b2ad9609212019ec0
Kenichi Takahashi <kenichi.takahashi@kabuku.co.jp>
2018-02-07 21:43:35 +0900

    part 3-1:
    - add Ball object.

---
+++ Assets/BallControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+    public GameObject ballObject;
+    public CatControl catControl;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount == 1) {
+            var touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began) {
+                Vector3 pos = touch.position;
+                pos.z = Camera.main.nearClipPlane * 2.0f;
+                var position = Camera.main.ScreenToWorldPoint (pos);
+                Instantiate (ballObject, position, Quaternion.identity);
+            }
+        }
+	}
+}
